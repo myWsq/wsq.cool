fa1a83487202009b021a13e7e2a4eaac79e13f10
myWsq <wsq961@outlook.com>
2021-11-10 22:08:34 +0800

    feat: 添加提示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-5xl">🚧 持续建设中...</h1>
+    </div>
+  );
 };
 
 export default Home;
